e0bfa9e2012837abc0231e33b79291f05ee735f3
Aleksa24 <aleksaivanovic24444@gmail.com>
2020-11-21 16:02:45 +0100

    init commit

---
+++ src/main/java/cloud/springframework/gurukursmsscbrewery/GuruKursMsscBreweryApplication.java
@@ -0,0 +1,13 @@
+package cloud.springframework.gurukursmsscbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruKursMsscBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuruKursMsscBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cloud/springframework/gurukursmsscbrewery/GuruKursMsscBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.springframework.gurukursmsscbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuruKursMsscBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
